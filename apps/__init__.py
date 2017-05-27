bc09a2a6eb5fa22e6d59a57337c10a8718caad26
hrkim102 <hrkim102@gmail.com>
2017-05-28 02:43:51 +0900

    Create connection to mongodb

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from mongokit import Connection
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+connection = Connection(app.config['MONGODB_HOST'], app.config['MONGODB_PORT'])
 
 @app.route('/')
 def index():
